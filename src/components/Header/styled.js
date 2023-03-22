abda5c41b3a7f563ce2c6f4e0233faeff92d402e
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-22 18:54:43 +0100

    create Header component & implement LayoutButton in it

---
+++ src/components/Header/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeaderWrapper = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const HeaderTitle = styled.h1``;
