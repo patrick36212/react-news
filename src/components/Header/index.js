f973163d15cb661fbed118b5017c61616d5ef36d
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-22 18:54:39 +0100

    create Header component & implement LayoutButton in it

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import { HeaderTitle, HeaderWrapper } from "./styled";
+import LayoutButton from "./LayoutButton";
+
+const Header = () => (
+  <HeaderWrapper>
+    <HeaderTitle>gnNews</HeaderTitle>
+    <LayoutButton />
+  </HeaderWrapper>
+);
+
+export default Header;
