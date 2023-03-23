a01c27f34e68f432843968324717eba407632398
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-23 18:48:15 +0100

    create component news

--- src/features/News/index.js
+++ src/features/News/index.js
@@ -1,20 +1,26 @@
 import Main from "../../components/Main";
 import NewsList from "../../components/NewsList";
 import { useQuery } from "react-query";
+import { getCountryNews } from "./getCountryNews";
+import { useEffect, useState } from "react";
 
 const News = () => {
+  const [articles, setArticles] = useState([]);
   const countryCode = "pl";
 
   const { data } = useQuery(["countryNews", countryCode], () => {
     if (!!countryCode) {
+      return getCountryNews(countryCode);
     }
   });
 
-  return (
-    <Main>
-      <NewsList />
-    </Main>
-  );
+  useEffect(() => {
+    if (!!data) {
+      setArticles(data.articles);
+    }
+  }, [data]);
+
+  return <Main>{!!data && <NewsList data={articles} />}</Main>;
 };
 
 export default News;
