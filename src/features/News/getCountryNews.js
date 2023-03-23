e62c97e96111ab88611737b9c9f8b98f26ed3505
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-23 18:47:10 +0100

    fon for fetching news according to country code

---
+++ src/features/News/getCountryNews.js
@@ -0,0 +1,9 @@
+import { fetchFromAPI } from "../../core/fetchFromAPI";
+
+export const getCountryNews = (countryCode) =>
+  fetchFromAPI({
+    path: "/top-headlines",
+    params: {
+      country: countryCode,
+    },
+  });
