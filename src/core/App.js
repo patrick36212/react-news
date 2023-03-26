2de0b56adee088bad2f59be7db7ef9a6f5ae5e51
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-26 19:33:31 +0200

    add menu component and logic to it

--- src/core/App.js
+++ src/core/App.js
@@ -3,11 +3,13 @@ import { GlobalStyle } from "../theme/GlobalStyle";
 import { theme } from "../theme/theme";
 import Header from "../components/Header";
 import News from "../features/News";
+import SideMenu from "../components/SideMenu";
 
 const App = () => (
   <ThemeProvider theme={theme}>
     <GlobalStyle />
     <Header />
+    <SideMenu />
     <News />
   </ThemeProvider>
 );
