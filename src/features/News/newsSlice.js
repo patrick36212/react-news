a450244dada1e63330898e28a891373c86c13a34
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-27 21:36:38 +0200

    change useState for redux State

--- src/features/News/newsSlice.js
+++ src/features/News/newsSlice.js
@@ -4,16 +4,21 @@ const newsSlice = createSlice({
   name: "news",
   initialState: {
     countryCode: null,
+    articles: [],
   },
   reducers: {
     setCountryCode: (state, { payload: code }) => {
       state.countryCode = code;
     },
+    setArticles: (state, { payload: articles }) => {
+      state.articles = articles;
+    },
   },
 });
 
-export const { setCountryCode } = newsSlice.actions;
+export const { setCountryCode, setArticles } = newsSlice.actions;
 
 export const selectCountryCode = (state) => state.news.countryCode;
+export const selectArticles = (state) => state.news.articles;
 
 export default newsSlice.reducer;
