329492e4de2d331a1c7b66f93f864b1590ce5b4c
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-27 21:35:44 +0200

    add basic css to main

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
-const Main = styled.main``;
+const Main = styled.main`
+  max-height: calc(100vh - 192px);
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin: 96px 0;
+  padding: 20px 0;
+  background: #f5f5f5;
+  box-shadow: inset 4px 3px 3px rgba(61, 60, 60, 0.35);
+`;
 
 export default Main;
