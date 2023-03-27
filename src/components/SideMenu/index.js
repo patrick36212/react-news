044530ef039b39869a48c21bd3dc435f13847e31
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-27 21:37:04 +0200

    add styled components to menu

--- src/components/SideMenu/index.js
+++ src/components/SideMenu/index.js
@@ -1,21 +1,36 @@
 import countries from "./countries.json";
 import { useDispatch } from "react-redux";
 import { setCountryCode } from "../../features/News/newsSlice";
+import { nanoid } from "@reduxjs/toolkit";
+import {
+  CountryFlag,
+  CountryName,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  MenuWrapper,
+} from "./styled";
 
 const SideMenu = () => {
   const countriesList = countries;
   const dispatch = useDispatch();
 
   return (
-    <ul>
-      {countriesList.map((country) => (
-        <button onClick={() => dispatch(setCountryCode(country.code))}>
-          <li>
-            <img src={country.flag} /> <p>{country.name}</p>
-          </li>
-        </button>
-      ))}
-    </ul>
+    <MenuWrapper>
+      <MenuList>
+        {countriesList.map((country) => (
+          <MenuButton
+            key={nanoid()}
+            onClick={() => dispatch(setCountryCode(country.code))}
+          >
+            <MenuItem>
+              <CountryFlag src={country.flag} />{" "}
+              <CountryName>{country.name}</CountryName>
+            </MenuItem>
+          </MenuButton>
+        ))}
+      </MenuList>
+    </MenuWrapper>
   );
 };
 
