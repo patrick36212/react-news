2223477f016a75a54d3f04ab361dcaf483927756
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-28 20:58:37 +0200

    add colors object to theme

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -6,4 +6,12 @@ const breakpoints = {
 
 export const theme = {
   breakpoints,
+  colors: {
+    mainColor: "#f5f5f5",
+    secondaryColor: "#252525",
+    additionalColor: "#959595",
+    sideBackground: "#0b4dda",
+    mainBackground: "#000000",
+    secondaryBackground: "#d5d5d5",
+  },
 };
