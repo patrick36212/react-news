88091622923c69b82353420c2bb2ad9386cd8358
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-29 17:46:51 +0200

    create & style popup component for news

---
+++ src/components/Popup/index.js
@@ -0,0 +1,32 @@
+import {
+  NewsPopup,
+  NewsPopupWrapper,
+  PopupAddInfo,
+  PopupCloseButton,
+  PopupContent,
+  PopupImg,
+} from "./styled";
+
+const Popup = ({ news, handleClose }) => {
+  const content =
+    news.content === null ? "For more information" : news.content.split("[")[0];
+
+  return (
+    <NewsPopupWrapper>
+      <NewsPopup>
+        {!!news.urlToImage && <PopupImg image={news.urlToImage} />}
+        <h2>{news.title}</h2>
+        <PopupContent>
+          {content}{" "}
+          <a href={news.url} target="_blank" rel="noopener noreferrer">
+            {news.content ? "read more" : "click"}
+          </a>
+        </PopupContent>
+        <PopupAddInfo>Author: {news.author}</PopupAddInfo>
+        <PopupCloseButton onClick={handleClose}>Close</PopupCloseButton>
+      </NewsPopup>
+    </NewsPopupWrapper>
+  );
+};
+
+export default Popup;
---
+++ src/components/Popup/styled.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+export const NewsPopupWrapper = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100vh;
+  background: ${({ theme }) => theme.colors.mainBackground}03;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 2;
+`;
+export const NewsPopup = styled.dialog`
+  max-width: 60%;
+  display: grid;
+  grid-auto-rows: max-content;
+  justify-items: center;
+  align-content: center;
+  grid-gap: 10px;
+  border: none;
+  background: ${({ theme }) => theme.colors.secondaryBackground};
+  overflow-y: auto;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    max-width: 80%;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    max-width: 95%;
+  }
+`;
+
+export const PopupImg = styled.div`
+  width: 100%;
+  height: 400px;
+  background-image: ${({ image }) => `url(${image})`};
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    height: 200px;
+  }
+`;
+
+export const PopupContent = styled.p`
+  text-align: justify;
+  margin: 0;
+  font-size: 16px;
+  line-height: 24px;
+`;
+
+export const PopupAddInfo = styled.span`
+  justify-self: right;
+  margin: 10px 0;
+  color: ${({ theme }) => theme.colors.secondaryColor};
+  font-size: 14px;
+`;
+
+export const PopupCloseButton = styled.button`
+  display: flex;
+  justify-content: center;
+  border: none;
+  font-weight: 900;
+  color: ${({ theme }) => theme.colors.mainColor};
+  background: ${({ theme }) => theme.colors.mainBackground};
+  cursor: pointer;
+  transition: 0.3s ease-out;
+
+  &:hover {
+    transform: scale(1.2);
+    background: crimson;
+    color: ${({ theme }) => theme.colors.secondaryColor};
+  }
+`;
