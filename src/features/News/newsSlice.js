6297bb236182609568842f38c3741431c9bf718e
Patryk Krawczyk <patrick36212@gmail.com>
2023-03-30 16:50:22 +0200

    add state and actions for popup

--- src/features/News/newsSlice.js
+++ src/features/News/newsSlice.js
@@ -5,6 +5,7 @@ const newsSlice = createSlice({
   initialState: {
     countryCode: null,
     articles: [],
+    selectedArticle: null,
   },
   reducers: {
     setCountryCode: (state, { payload: code }) => {
@@ -13,12 +14,27 @@ const newsSlice = createSlice({
     setArticles: (state, { payload: articles }) => {
       state.articles = articles;
     },
+    setSelectedArticle: (state, { payload: articleId }) => {
+      const articleIndex = state.articles.findIndex(
+        ({ id }) => id === articleId
+      );
+      state.selectedArticle = state.articles[articleIndex];
+    },
+    closeSelectedArticle: (state) => {
+      state.selectedArticle = null;
+    },
   },
 });
 
-export const { setCountryCode, setArticles } = newsSlice.actions;
+export const {
+  setCountryCode,
+  setArticles,
+  setSelectedArticle,
+  closeSelectedArticle,
+} = newsSlice.actions;
 
 export const selectCountryCode = (state) => state.news.countryCode;
 export const selectArticles = (state) => state.news.articles;
+export const selectSelectedArticle = (state) => state.news.selectedArticle;
 
 export default newsSlice.reducer;
