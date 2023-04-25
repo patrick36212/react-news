c715606e1d5c1c57495d2832a3b7b86de85b6e7f
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-25 18:14:47 +0200

    create section component & add CSS to it

---
+++ src/components/Section/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Section = styled.section`
+  flex-grow: 1;
+  display: inherit;
+  justify-content: center;
+  align-content: center;
+`;
+
+export default Section;
