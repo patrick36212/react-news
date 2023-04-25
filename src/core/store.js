c2fd29c835fda1c59503d9355d637ba67695c0fa
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-25 18:16:54 +0200

    refactor import

--- src/core/store.js
+++ src/core/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import layoutReducer from "../components/Header/LayoutButton/layoutSlice";
-import newsReducer from "../features/News/newsSlice";
+import newsReducer from "../features/newsSlice";
 
 const store = configureStore({
   reducer: {
