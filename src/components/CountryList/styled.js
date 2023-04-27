0ecdc772cf81de9eb6a4c3d14ba160b8077146a7
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-27 19:23:37 +0200

    change css for sidebar to be more universal in devices

--- src/components/CountryList/styled.js
+++ src/components/CountryList/styled.js
@@ -1,30 +1,33 @@
-import styled, { css } from "styled-components";
-import { NavLink } from "react-router-dom";
-
+import styled from "styled-components";
+import {NavLink} from "react-router-dom";
 export const ListWrapper = styled.aside`
-  min-width: max-content;
-  max-width: max-content;
-  max-height: calc(100vh - 182px);
-  padding: 18px;
-  background: ${({ theme }) => theme.colors.sideBackground};
+  position: absolute;
+  max-height: calc(100vh - 98px);
+  width: 60px;
+  padding: 10px 10px 30px 10px;
+  background: ${({theme}) => theme.colors.sideBackground};
   display: flex;
   justify-content: center;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
-    min-width: 220px;
+  transition: all .5s ease-in-out;
+  
+  p {
+    opacity: 0;
   }
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-    min-width: 100%;
-    transition: 0.3s ease-in-out;
-    transform: translateX(-100%);
-    z-index: 1;
+  &:hover {
+    width: 230px;
+    
+    @media(max-width: ${({theme}) => theme.breakpoints.mobile}px) {
+      width: 100%;
+    }
+    
+    p {
+      opacity: 1;
+    }
+  }
 
-    ${({ active }) =>
-      active &&
-      css`
-        transform: translateX(0);
-      `}
+  @media(max-width: ${({theme}) => theme.breakpoints.tablet}px) {
+    max-height: calc(100vh - 78px);
   }
 `;
 export const List = styled.ul`
@@ -41,36 +44,30 @@ export const List = styled.ul`
   }
 `;
 export const ListButton = styled(NavLink)`
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
+  display: grid;
+  justify-items: start;
+  grid-template-columns: 20px 1fr;
   gap: 6px;
   text-decoration: none;
-  color: ${({ theme }) => theme.colors.mainColor};
+  color: ${({theme}) => theme.colors.mainColor};
   cursor: pointer;
   transition: all 0.3s ease-in-out;
   padding: 10px;
 
-  &:hover {
-    background: ${({ theme }) => theme.colors.mainColor};
-    color: ${({ theme }) => theme.colors.secondaryColor};
-    transform: scale(1.04);
-  }
-
-  &.active {
-    background: ${({ theme }) => theme.colors.mainColor};
-    color: ${({ theme }) => theme.colors.secondaryColor};
+  &:hover, &.active {
+    background: ${({theme}) => theme.colors.mainColor};
+    color: ${({theme}) => theme.colors.secondaryColor};
   }
 `;
 
 export const CountryFlag = styled.div`
   width: 20px;
   height: 20px;
-  background-image: ${({ src }) => `url(${src})`};
-  background-size: contain;
+  background-image: ${({src}) => `url(${src})`};
+  background-size: cover;
   background-repeat: no-repeat;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+  @media (max-width: ${({theme}) => theme.breakpoints.tablet}px) {
     width: 16px;
     height: 16px;
   }
@@ -78,30 +75,12 @@ export const CountryFlag = styled.div`
 
 export const CountryName = styled.p`
   font-size: 16px;
+  white-space: nowrap;
+  height: 20px;
   margin: 0;
 
-  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+  @media (max-width: ${({theme}) => theme.breakpoints.tablet}px) {
     font-size: 14px;
-  }
-`;
-
-export const OpenCountryList = styled.button`
-  display: none;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
-    border: none;
-    position: absolute;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-weight: 600;
-    color: ${({ theme }) => theme.colors.mainColor};
-    cursor: pointer;
-    top: 50%;
-    border-radius: 0 12px 12px 0;
-    width: 30px;
-    height: 30px;
-    background: ${({ theme }) => theme.colors.sideBackground};
-    z-index: 1;
+    height: 16px;
   }
 `;
