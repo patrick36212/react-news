deaf8ca72a8bd3130d06707fecbbd74824c348fb
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-27 19:56:59 +0200

    add static height prop

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,10 +1,16 @@
 import styled from "styled-components";
 
 const Section = styled.section`
-  flex-grow: 1;
   display: inherit;
   justify-content: center;
-  align-content: center;
+  align-items: center;
+  padding: 0 0 0 60px;
+  width: 100%;
+  height: calc(100vh - 180px);
+  
+  @media(max-width: ${({theme}) => theme.breakpoints.tablet}px) {
+    height: calc(100vh - 125px);
+  }
 `;
 
 export default Section;
