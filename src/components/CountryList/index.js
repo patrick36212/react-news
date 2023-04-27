a5380196b08d017f8628c89d23261c8b47fd0f3d
Patryk Krawczyk <patrick36212@gmail.com>
2023-04-27 19:23:57 +0200

    delete unnecessary components

--- src/components/CountryList/index.js
+++ src/components/CountryList/index.js
@@ -2,30 +2,25 @@ import countries from "./countries.json";
 import { useDispatch } from "react-redux";
 import { setCountryCode } from "../../features/newsSlice";
 import { nanoid } from "@reduxjs/toolkit";
-import { useState } from "react";
 import {
   CountryFlag,
   CountryName,
   List,
   ListButton,
   ListWrapper,
-  OpenCountryList,
 } from "./styled";
 
 const CountryList = () => {
   const countriesList = countries;
   const dispatch = useDispatch();
 
-  const [isVisible, setVisible] = useState(false);
-
   const handleOnClick = (country) => {
     dispatch(setCountryCode(country.code));
-    setVisible(false);
   };
 
   return (
     <>
-      <ListWrapper active={isVisible}>
+      <ListWrapper>
         <List>
           {countriesList.map((country) => (
             <ListButton
@@ -39,9 +34,6 @@ const CountryList = () => {
           ))}
         </List>
       </ListWrapper>
-      <OpenCountryList onClick={() => setVisible(!isVisible)}>
-        {isVisible ? "<" : ">"}
-      </OpenCountryList>
     </>
   );
 };
