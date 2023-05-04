bfa5e2cea8c541920be92a5fc96f6e9bf4397ddf
Patryk Krawczyk <patrick36212@gmail.com>
2023-05-04 16:45:55 +0200

    add fullData state and action for it

--- src/features/newsSlice.js
+++ src/features/newsSlice.js
@@ -4,12 +4,16 @@ const newsSlice = createSlice({
   name: "news",
   initialState: {
     countryCode: null,
+    fullData: null,
     articles: [],
     selectedArticle: null,
   },
   reducers: {
     setCountryCode: (state, { payload: code }) => {
       state.countryCode = code;
+    },
+    setFullData: (state, { payload: data }) => {
+      state.fullData = data;
     },
     setArticles: (state, { payload: articles }) => {
       state.articles = articles;
@@ -31,10 +35,12 @@ export const {
   setArticles,
   setSelectedArticle,
   closeSelectedArticle,
+  setFullData
 } = newsSlice.actions;
 
 export const selectCountryCode = (state) => state.news.countryCode;
 export const selectArticles = (state) => state.news.articles;
 export const selectSelectedArticle = (state) => state.news.selectedArticle;
+export const selectFullData = state => state.news.fullData;
 
 export default newsSlice.reducer;
