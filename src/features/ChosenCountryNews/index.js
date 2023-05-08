be63ee5811a7d9295fc37ecd0ad57743c9edb492
Patryk Krawczyk <patrick36212@gmail.com>
2023-05-08 19:10:29 +0200

    add news Wrapper, implement changed states and actions

--- src/features/ChosenCountryNews/index.js
+++ src/features/ChosenCountryNews/index.js
@@ -2,10 +2,11 @@ import Loading from "../../components/Info/Loading";
 import Error from "../../components/Info/Error";
 import NewsList from "../../components/NewsList";
 import Popup from "../../components/Popup";
+import News from "../index";
 import {
   closeSelectedArticle,
   selectArticles,
-  selectCountryCode,
+  selectCountry,
   selectSelectedArticle,
   setArticles,
   setFullData,
@@ -15,21 +16,18 @@ import { useQuery } from "react-query";
 import { getNewsData } from "../getNewsData";
 import { nanoid } from "@reduxjs/toolkit";
 import { useEffect } from "react";
-import News from "../index";
-import {useLocation} from "react-router";
 
 const ChosenCountryNews = () => {
   const dispatch = useDispatch();
-  const location = useLocation();
-  const countryCode = useSelector(selectCountryCode);
+  const country = useSelector(selectCountry);
   const articles = useSelector(selectArticles);
   const selectedArticle = useSelector(selectSelectedArticle);
 
   const { data, isError, isLoading } = useQuery(
-    ["countryNews", countryCode],
+    ["countryNews", country],
     () => {
-      if (!!countryCode) {
-        return getNewsData(countryCode);
+      if (!!country) {
+        return getNewsData(country.code);
       }
     }
   );
@@ -48,11 +46,7 @@ const ChosenCountryNews = () => {
     <News>
       {!!isLoading && <Loading />}
       {!!isError && <Error />}
-      {!!data &&
-      <>
-        <h2>Latest news for {location.pathname.split("/")[3].replaceAll("%20", " ")}</h2>
-        <NewsList data={articles} />
-      </>}
+      {!!data && <NewsList country={country} data={articles} />}
       {!!selectedArticle && (
         <Popup
           news={selectedArticle}
