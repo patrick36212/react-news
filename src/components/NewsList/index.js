9797d3ae6f5d9068f616dbd9fb726eb469f33540
Patryk Krawczyk <patrick36212@gmail.com>
2023-05-08 19:11:09 +0200

    add title and props to it

--- src/components/NewsList/index.js
+++ src/components/NewsList/index.js
@@ -1,6 +1,7 @@
 import {
   Image,
   InfoWrapper,
+  NewsSectionTitle,
   NewsTileInfo,
   NewsTileList,
   NewsTileTitle,
@@ -11,36 +12,39 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectIsListActive } from "../Header/LayoutButton/layoutSlice";
 import { setSelectedArticle } from "../../features/newsSlice";
 
-const NewsList = ({ data }) => {
+const NewsList = ({ data, country }) => {
   const isListActive = useSelector(selectIsListActive);
   const dispatch = useDispatch();
 
   return (
-    <NewsTileList list={isListActive}>
-      {data.map((news) => (
-        <TileButton
-          onClick={() => dispatch(setSelectedArticle(news.id))}
-          key={news.id}
-        >
-          <TileWrapper list={isListActive}>
-            <Image
-              image={news.image_url}
-              noImage={!news.image_url}
-              list={isListActive}
-            />
-            <NewsTileTitle title={news.title} list={isListActive}>
-              {news.title}
-            </NewsTileTitle>
-            <InfoWrapper list={isListActive}>
-              <NewsTileInfo>Source: {news.source_id}</NewsTileInfo>
-              <NewsTileInfo>
-                Published at: {news.pubDate}
-              </NewsTileInfo>
-            </InfoWrapper>
-          </TileWrapper>
-        </TileButton>
-      ))}
-    </NewsTileList>
+    <>
+      {country && (
+        <NewsSectionTitle>Latest news for {country.name}</NewsSectionTitle>
+      )}
+      <NewsTileList list={isListActive}>
+        {data.map((news) => (
+          <TileButton
+            onClick={() => dispatch(setSelectedArticle(news.id))}
+            key={news.id}
+          >
+            <TileWrapper list={isListActive}>
+              <Image
+                image={news.image_url}
+                noImage={!news.image_url}
+                list={isListActive}
+              />
+              <NewsTileTitle title={news.title} list={isListActive}>
+                {news.title}
+              </NewsTileTitle>
+              <InfoWrapper list={isListActive}>
+                <NewsTileInfo>Source: {news.source_id}</NewsTileInfo>
+                <NewsTileInfo>Published at: {news.pubDate}</NewsTileInfo>
+              </InfoWrapper>
+            </TileWrapper>
+          </TileButton>
+        ))}
+      </NewsTileList>
+    </>
   );
 };
 
