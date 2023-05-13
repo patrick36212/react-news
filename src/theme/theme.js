f33e7908a2668d391f19c8c630c8153fcb9ba9b9
Patryk Krawczyk <patrick36212@gmail.com>
2023-05-13 19:52:40 +0200

    change colors, add additionalBackground

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -7,12 +7,12 @@ const breakpoints = {
 export const theme = {
   breakpoints,
   colors: {
-    mainColor: "#f5f5f5",
-    secondaryColor: "#252525",
+    mainColor: "#252525",
+    secondaryColor: "#f5f5f5",
     additionalColor: "#959595",
-    sideBackground: "#0b4dda",
-    mainBackground: "#000000",
-    secondaryBackground: "#d5d5d5",
-    backdropBackground: "#0B4DDA56",
+    sideBackground: "#4f74cc",
+    mainBackground: "#d5d5d5",
+    secondaryBackground: "#000000",
+    additionalBackground: "#efefef",
   },
 };
