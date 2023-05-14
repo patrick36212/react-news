93d1e658d0e154d6b92a3f326167356b419552bf
Patryk Krawczyk <patrick36212@gmail.com>
2023-05-14 18:59:13 +0200

    refactor for Search, add input

---
+++ src/components/Header/Search/index.js
@@ -0,0 +1,59 @@
+import countries from "./countries.json";
+import { useDispatch } from "react-redux";
+import { setCountry } from "../../../features/newsSlice";
+import { nanoid } from "@reduxjs/toolkit";
+import {
+  CountryFlag,
+  CountryName,
+  Input,
+  List,
+  ListButton,
+  SearchWrapper,
+} from "./styled";
+import { useState } from "react";
+
+const Search = () => {
+  const dispatch = useDispatch();
+  const [query, setQuery] = useState("");
+
+  const filterCountryByQuery = () => {
+    if (!query || query.trim() === "") {
+      return;
+    }
+    return countries.filter(({ name }) =>
+      name.toUpperCase().includes(query.toUpperCase())
+    );
+  };
+
+  const countriesList = filterCountryByQuery();
+  const handleOnClick = (country) => {
+    dispatch(setCountry(country));
+    setQuery("");
+  };
+
+  return (
+    <SearchWrapper>
+      <Input
+        searched={!!query}
+        placeholder="Which country?"
+        value={query}
+        onChange={({ target }) => setQuery(target.value)}
+      />
+      <List>
+        {query &&
+          countriesList.slice(0, 10).map((country) => (
+            <ListButton
+              to={`/news/country/${country.name}`}
+              key={nanoid()}
+              onClick={() => handleOnClick(country)}
+            >
+              <CountryFlag src={country.flag} />{" "}
+              <CountryName>{country.name}</CountryName>
+            </ListButton>
+          ))}
+      </List>
+    </SearchWrapper>
+  );
+};
+
+export default Search;
