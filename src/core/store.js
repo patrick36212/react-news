60c6474e099a3eb46636e8532ccd5ff97c5713ac
Patryk Krawczyk <patrick36212@gmail.com>
2023-05-17 14:44:30 +0200

    delete layout buttons and slice

--- src/core/store.js
+++ src/core/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import layoutReducer from "../components/Header/LayoutButton/layoutSlice";
 import newsReducer from "../features/newsSlice";
 
 const store = configureStore({
   reducer: {
-    layout: layoutReducer,
     news: newsReducer,
   },
 });
