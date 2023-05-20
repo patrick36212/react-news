c42aa279542c255821bbeb29da5a0680f8c09c31
Patryk Krawczyk <patrick36212@gmail.com>
2023-05-20 22:40:43 +0200

    set max width foe main, del box-shadow

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,8 +2,8 @@ import styled from "styled-components";
 
 const Main = styled.main`
   display: flex;
-  background: ${({ theme }) => theme.colors.secondaryBackground};
-  box-shadow: inset 0 3px 3px rgba(61, 60, 60, 0.35);
+  flex-direction: column;
+  max-width: ${({ theme }) => theme.breakpoints.laptop}px;
 `;
 
 export default Main;
