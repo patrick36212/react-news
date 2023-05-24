9993e8a2fd9deef851f7b0ad8f492a9d5cb39489
Patryk Krawczyk <patrick36212@gmail.com>
2023-05-24 15:51:16 +0200

    change display & add props for news

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -1,10 +1,28 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const Wrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
+  display: grid;
+  justify-items: center;
+  align-content: center;
+
+  ${({ news }) =>
+    news &&
+    css`
+      border-radius: 6px;
+      width: 100%;
+      grid-gap: 8px;
+      grid-template-columns: repeat(2, 1fr);
+      grid-auto-rows: 120px;
+
+      article:first-of-type {
+        grid-column: 1;
+        grid-row: 1/4;
+      }
+      article:last-of-type {
+        grid-column: 2;
+        grid-row: 5/8;
+      }
+    `}
 `;
 
 export default Wrapper;
