70fd36f767ae0f6e0d1217e099eef183c3a959eb
Patryk Krawczyk <patrick36212@gmail.com>
2023-05-24 15:50:53 +0200

    change div in section for customized wrapper

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,15 +1,16 @@
 import { SectionWrapper } from "./styled";
 import Tile from "../Tile";
+import Wrapper from "../Wrapper";
 
 const Section = ({ title, newsData }) => {
   return (
     <SectionWrapper>
       <h2>{title}</h2>
-      <div>
+      <Wrapper news="true">
         {newsData.map((news) => (
           <Tile data={news} />
         ))}
-      </div>
+      </Wrapper>
     </SectionWrapper>
   );
 };
