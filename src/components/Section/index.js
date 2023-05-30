39d1f80ec8f52da8582eda6f89b585f437c23b1a
Patryk Krawczyk <patrick36212@gmail.com>
2023-05-30 18:55:34 +0200

    made section reusable component

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -2,15 +2,11 @@ import { SectionWrapper } from "./styled";
 import Tile from "../Tile";
 import Wrapper from "../Wrapper";
 
-const Section = ({ title, newsData }) => {
+const Section = ({ title, sectionNews }) => {
   return (
     <SectionWrapper>
       <h2>{title}</h2>
-      <Wrapper news="true">
-        {newsData.map((news) => (
-          <Tile data={news} />
-        ))}
-      </Wrapper>
+      <Wrapper news="true">{sectionNews}</Wrapper>
     </SectionWrapper>
   );
 };
