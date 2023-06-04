b62e251e4c5dda8a4d1c39cc7b72cede822df2d0
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-04 19:19:58 +0200

    delete main component from news

--- src/features/index.js
+++ src/features/index.js
@@ -1,7 +1,6 @@
 import Wrapper from "../components/Wrapper";
 import Heading from "../components/Header";
 import Footer from "../components/Footer";
-import Main from "../components/Main";
 
 const News = ({ children }) => {
   return (
