f24c69a3e9428351cf830c0151ace1ee6eab6e85
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-13 19:54:24 +0200

    del title marker from section

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,10 +1,9 @@
 import { SectionWrapper } from "./styled";
 import Wrapper from "../Wrapper";
 
-const Section = ({ title, sectionNews }) => {
+const Section = ({ sectionNews }) => {
   return (
     <SectionWrapper>
-      <h2>{title}</h2>
       {!!sectionNews && <Wrapper news="true">{sectionNews}</Wrapper>}
     </SectionWrapper>
   );
