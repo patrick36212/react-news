2757e91635ac511fed58037f42ee8a13fd1fd593
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-13 20:08:43 +0200

    create path for article, add empty component to it

--- src/core/routes.js
+++ src/core/routes.js
@@ -6,16 +6,19 @@ import {
 } from "react-router-dom";
 import GeoCountryNews from "../features/GeoCountryNews";
 import ChosenCountryNews from "../features/ChosenCountryNews";
+import ChosenArticle from "../features/ChosenArticle";
 
-export const geoCountryNews = "/news";
+export const mainPage = "/news";
 export const countryPath = "/news/:countryCode";
+export const article = "/news/:countryCode/:articleId";
 
 export const routes = createHashRouter(
   createRoutesFromElements(
     <>
-      <Route path={geoCountryNews} element={<GeoCountryNews />} />
+      <Route path={mainPage} element={<GeoCountryNews />} />
       <Route path={countryPath} element={<ChosenCountryNews />} />
-      <Route path="/" element={<Navigate to={geoCountryNews} />} />
+      <Route path={article} element={<ChosenArticle />} />
+      <Route path="/" element={<Navigate to={mainPage} />} />
     </>
   )
 );
