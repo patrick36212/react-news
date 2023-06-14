52cc22a953e404faa454297fafbe5451fefc0612
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-14 18:20:56 +0200

    del margin

--- src/components/Footer/syled.js
+++ src/components/Footer/syled.js
@@ -1,7 +1,6 @@
 import styled from "styled-components";
 
 export const FooterWrapper = styled.footer`
-  margin: 20px 0 0 0;
   width: 100%;
   background: ${({ theme }) => theme.colors.footerBackground};
   color: ${({ theme }) => theme.colors.secondaryBackground};
