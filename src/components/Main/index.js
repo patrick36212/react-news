1474a6bfeef9036a6e3c9b86f044cba8a43f7bd1
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-14 17:06:22 +0200

    add static height props and center all content

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,9 +2,11 @@ import styled from "styled-components";
 
 const Main = styled.main`
   display: flex;
-  flex-direction: column;
+  justify-content: center;
+  align-items: center;
   max-width: ${({ theme }) => theme.breakpoints.laptop}px;
   width: 100%;
+  min-height: 85dvh;
 `;
 
 export default Main;
