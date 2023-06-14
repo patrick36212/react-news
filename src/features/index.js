aebbd5c4fc5aa717a56e79efc5e33dad9c64d91b
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-14 17:06:54 +0200

    add back Main component with min-height

--- src/features/index.js
+++ src/features/index.js
@@ -1,12 +1,13 @@
 import Wrapper from "../components/Wrapper";
 import Heading from "../components/Header";
 import Footer from "../components/Footer";
+import Main from "../components/Main";
 
 const News = ({ children }) => {
   return (
     <Wrapper>
       <Heading />
-      {children}
+      <Main>{children}</Main>
       <Footer />
     </Wrapper>
   );
