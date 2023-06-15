11f0fea361543d0b0535417a190c0683a274b92f
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-15 19:37:25 +0200

    add gap and direction

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -2,8 +2,10 @@ import styled from "styled-components";
 
 const Main = styled.main`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  gap: 20px;
   max-width: ${({ theme }) => theme.breakpoints.laptop}px;
   width: 100%;
   min-height: 85dvh;
