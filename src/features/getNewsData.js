2acaf7f955e72c519ffe312eeb933210aea7de71
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-18 19:05:49 +0200

    add additional prop to fetching fn

--- src/features/getNewsData.js
+++ src/features/getNewsData.js
@@ -1,9 +1,10 @@
 import { fetchFromAPI } from "../core/fetchFromAPI";
 
-export const getNewsData = (countryCode) =>
+export const getNewsData = (countryCode, page) =>
   fetchFromAPI({
     path: "/news",
     params: {
       country: countryCode,
+      page: page,
     },
   });
