10efd056f7179354979e6452e56358114f662066
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-19 19:32:17 +0200

    del unnecessary border

--- src/components/Section/styled.js
+++ src/components/Section/styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const SectionWrapper = styled.section`
   display: grid;
-  border-bottom: 1px solid ${({ theme }) => theme.colors.secondaryBackground};
   max-width: ${({ theme }) => theme.breakpoints.laptop}px;
   margin: auto;
   width: 100%;
