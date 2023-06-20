6cc07a515ad76177da2065e8a0d4071cc7547867
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-20 18:52:47 +0200

    create & export pageHistory and pageNumber state & actions

--- src/features/newsSlice.js
+++ src/features/newsSlice.js
@@ -6,6 +6,8 @@ const newsSlice = createSlice({
     countryCode: null,
     articles: [],
     selectedArticle: null,
+    pageHistory: [{ page: 1, pageCode: null }],
+    pageNumber: 1,
   },
   reducers: {
     setCountryCode: (state, { payload: code }) => {
@@ -20,6 +22,12 @@ const newsSlice = createSlice({
     clearSelectedArticle: (state) => {
       state.selectedArticle = null;
     },
+    setPageHistory: (state, { payload: page }) => {
+      state.pageHistory.push(page);
+    },
+    setPageNumber: (state, action) => {
+      state.pageNumber = action.payload;
+    },
   },
 });
 
@@ -28,10 +36,14 @@ export const {
   setArticles,
   setSelectedArticle,
   clearSelectedArticle,
+  setPageHistory,
+  setPageNumber,
 } = newsSlice.actions;
 
 export const selectCountryCode = (state) => state.news.countryCode;
 export const selectArticles = (state) => state.news.articles;
 export const selectSelectedArticle = (state) => state.news.selectedArticle;
+export const selectPageHistory = (state) => state.news.pageHistory;
+export const selectPageNumber = (state) => state.news.pageNumber;
 
 export default newsSlice.reducer;
