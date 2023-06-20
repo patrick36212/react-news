accc06ab8ec107bdc10d13fbadf0a188513e4525
Patryk Krawczyk <patrick36212@gmail.com>
2023-06-20 18:50:41 +0200

    add info what is actual page

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,11 +1,12 @@
 import { PaginationButton, PaginationWrapper } from "./styled";
 
-const Pagination = ({ backFn, nextFn, disabledFn }) => {
+const Pagination = ({ backFn, nextFn, disabledFn, pageNumber }) => {
   return (
     <PaginationWrapper>
       <PaginationButton disabled={disabledFn} onClick={backFn}>
         {"<"}
       </PaginationButton>
+      <p>Page {pageNumber}</p>
       <PaginationButton next="true" onClick={nextFn}>
         {"Next >"}
       </PaginationButton>
